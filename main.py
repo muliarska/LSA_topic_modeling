24d54ce46c2a633ece37d6b6b1f918cba575c694
Sofia Kyba <56972348+Sofia-Kyba@users.noreply.github.com>
2021-04-22 15:48:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ filename = "mini_newsgroups.tar.gz"
 extension = "r:gz"
 all_words = process_data(filename, extension)
 # print(all_words[:10])
+
+# поки для швидшого виконання обрізала дані
+all_words = all_words[:10]
+
+constructed_dtm = form_dtm(all_words)
+print(constructed_dtm)
