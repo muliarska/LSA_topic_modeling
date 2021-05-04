28703f463c88a6054621137e8bbe33a68a25e385
muliarska <yana.muliarska@gmail.com>
2021-05-04 20:32:40 +0300

    fixed SVD

--- modules/main.py
+++ modules/main.py
@@ -7,18 +7,17 @@ extension = "r:gz"
 all_words, file_names = process_data(filename, extension)
 # print(all_words[:10])
 
-print(file_names)
-print(len(file_names))
 
 # поки для швидшого виконання обрізала дані
-all_words = all_words[:2]
+all_words = all_words[:10]
 
 # constructing document term matrix
 constructed_tdm = form_tdm(all_words)
 
-print(constructed_tdm)
+# print(constructed_tdm)
 # applying tf_idf algorithm
 modified_tdm = tf_idf_modification(constructed_tdm)
 
-print(modified_tdm)
-#apply_svd(modified_tdm)
+# print(modified_tdm)
+topics_numb = 9 # ???
+apply_svd(modified_tdm, file_names[:10], topics_numb)
--- modules/svd.py
+++ modules/svd.py
@@ -1,18 +1,17 @@
 from sklearn.decomposition import TruncatedSVD
 
 
-def apply_svd(data):
-    svd_model = TruncatedSVD(n_components=20, algorithm='randomized', n_iter=100, random_state=122)
-    terms = data[0]
-    svd_model.fit(data[1:])
+def apply_svd(data, docs, topics_numb):
+    svd_model = TruncatedSVD(n_components=topics_numb, algorithm='randomized', n_iter=100, random_state=122)
+    svd_model.fit(data)
 
     # print(len(svd_model.components_))
 
     for i, comp in enumerate(svd_model.components_):
-        terms_comp = zip(terms, comp)
-        sorted_terms = sorted(terms_comp, key=lambda x: x[1], reverse=True)[:7]
+        docs_comp = zip(docs, comp)
+        sorted_docs = sorted(docs_comp, key=lambda x: x[1], reverse=True)[:7]
         temp_str = "Topic " + str(i) + ": "
-        for t in sorted_terms:
+        for t in sorted_docs:
             temp_str += t[0] + " "
         temp_str += "\n"
         print(temp_str)
